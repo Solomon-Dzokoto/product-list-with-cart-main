8ac4bc41b9b82f063ba2fe754bafda438fd8a970
Solomon-Dzokoto <dzokotoslomon85@gmail.com>
2024-07-19 21:09:24 +0000

    initial changes

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
 const buttons=document.querySelectorAll('.button');
 
-buttons.forEach(button=>
-  button.addEventListener('click',()=>{
-   const image=buttun.dataset.imageId
-   image.style.border='1px solid red'
-  };
-)
-);
+
+buttons.forEach((button=>{
+  button.add.EventListener('click',()=>{
+    
+  })
+}))
+
+
+document.querySelector('.js-main-container').innerHTML=productHTML
+let myButton=document.querySelectorAll('.button') ;
+ const foodImage=document.querySelectorAll('.js-food-image');
+ const foodName=document.querySelectorAll('.food-name')
+ const productName=product.name;
+ 
+
+function addToCard(productId){
+  let matchingItem;
+cart.forEach((item)=>{
+if(productName===item.productName){
+  matchingItem = item;
+} 
+});
+if (matchingItem){
+  matchingItem.quantity +=1;
+}else{
+  cart.push({
+    productName:productName,
+    quantity:1
+  })
+}
+};
+
+
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+  
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+  
+    document.querySelector('.js-cart-number')
+      .innerHTML = cartQuantity;
+  }
+  
+  document.querySelectorAll('.button')
+    .forEach((button) => {
+      button.addEventListener('click', () => {
+        const productId = button.dataset.productId;
+        addToCart(productId);
+        updateCartQuantity();
+      });
+    });
